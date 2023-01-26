e2b6a3e294847be84a8b65713482b2e9607595c3
kilik42 <maevins@cps.edu>
2023-01-26 04:43:05 -0600

    working on maps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import { useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
+import* as  tt from '@tomtom-international/web-sdk-maps';
 
 function App() {
   const [map, setMap] = useState({});
-
+  const mapElement = useRef(null);
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOMTOM_API_KEY,
@@ -16,7 +18,7 @@ function App() {
       }, []); 
   return (
     <div className="App">
-      
+      <div ref={mapElement}></div>
 
     </div>
   );
