b78e3968f54659b8f323370773ace95558c234d5
kilik42 <maevins@cps.edu>
2023-01-26 04:50:24 -0600

    working on maps

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ import* as  tt from '@tomtom-international/web-sdk-maps';
 function App() {
   const [map, setMap] = useState({});
   const mapElement = useRef(null);
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOMTOM_API_KEY,
       container: mapElement,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 14,
+
     });
 
     setMap(map);
 
       }, []); 
   return (
-    <div className="App">
-      <div ref={mapElement}></div>
+    <div className="app">
+      <div ref={mapElement} className = "map"></div>
 
     </div>
   );
